adaa5eb1c7ae57dccc6842b3afa01f514ed71aaf
Sara <sara@Saras-MacBook-Pro.local>
2025-01-20 12:26:31 +0000

    Add TanStack Query as peer dependency

--- src/app/core/hooks/AppProvider.tsx
+++ src/app/core/hooks/AppProvider.tsx
@@ -12,8 +12,8 @@ import { useSentry } from "./useSentry";
 
 import { ModalProvider } from "../context/ModalContext";
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
