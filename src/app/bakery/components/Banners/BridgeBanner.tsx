6e6b5584c925c9aae4df74aae8bca09e7fda4c67
subject026 <subject026@protonmail.com>
2025-01-28 11:08:32 +0000

    add new liquidity banner + refac bridge banner to make use of shared components

--- src/app/bakery/components/Banners/BridgeBanner.tsx
+++ src/app/bakery/components/Banners/BridgeBanner.tsx
@@ -8,21 +8,13 @@ import {
   BannerDescription,
   BannerHighlight,
   ArrowIcon,
+  BannerLargeTextContainer,
 } from "./Shared";
 
 const jumpLink = "https://app.debridge.finance/?outputChain=100";
 
-const renderGnosisLogo = () => {
-  return (
-    <Image
-      src="/gnosis-logo-bg.png"
-      alt="Gnosis logo"
-      className="absolute top-0 left-0 w-auto h-auto duration-300 group-hover:opacity-100 opacity-0 transition-all z-0"
-      width="222"
-      height="191"
-    ></Image>
-  );
-};
+const BANNER_TITLE = "Gnosis Chain token bridge";
+const BANNER_DESCRIPTION = "Deposit tokens to the Gnosis Chain network";
 
 export function BridgeBanner() {
   return (
@@ -30,43 +22,47 @@ export function BridgeBanner() {
       {/* large */}
       <a href={jumpLink} target="_blank" rel="noopener noreferrer">
         <BannerCardLarge>
-          {renderGnosisLogo()}
-          {/* First Column: Gnosis Icon */}
-          <div className="w-[10%] flex justify-center items-center">
+          <GnosisLogoBackground />
+          <div className="w-[3rem] flex justify-center items-center">
             <GnosisIcon />
           </div>
-          {/* Second Column: Text */}
-          <div className="w-[85%]">
-            <BannerTitle>Gnosis Chain token bridge</BannerTitle>
-            <BannerDescription>
-              Deposit tokens to the Gnosis Chain network
-            </BannerDescription>
-          </div>
+          <BannerLargeTextContainer>
+            <BannerTitle>{BANNER_TITLE}</BannerTitle>
+            <BannerDescription>{BANNER_DESCRIPTION}</BannerDescription>
+          </BannerLargeTextContainer>
           {/* Third Column: Arrow */}
-          <div className="w-[5%] flex justify-center items-center text-xl text-breadgray-grey300 dark:text-white transition-transform group-hover:translate-x-3">
+          <div className="flex justify-center items-center group-hover:text-breadviolet-shaded group-hover:dark:text-breadpink-300 text-breadgray-rye dark:text-breadgray-grey group-hover:translate-x-3 transition-all">
             <ArrowIcon />
-          </div>{" "}
+          </div>
         </BannerCardLarge>
       </a>
       {/* small */}
       <a href={jumpLink} target="_blank" rel="noopener noreferrer">
         <BannerCardSmall>
-          {renderGnosisLogo()}
-          {/* Title */}
-          <BannerTitle>Gnosis Chain token bridge</BannerTitle>
-
-          {/* Description */}
-          <BannerDescription>
-            Deposit tokens to the Gnosis Chain network
-          </BannerDescription>
-
-          {/* Icon Section */}
-
-          <BannerHighlight icon={<GnosisIcon size="small" />}>
+          <GnosisLogoBackground />
+          <BannerTitle>{BANNER_TITLE}</BannerTitle>
+          <BannerDescription>{BANNER_DESCRIPTION}</BannerDescription>
+          <BannerHighlight
+            variant="green"
+            featureIcon={<GnosisIcon size="small" />}
+            arrowIcon={<ArrowIcon />}
+          >
             Bridge to Gnosis
           </BannerHighlight>
         </BannerCardSmall>
       </a>
     </BannerContainer>
+  );
+}
+
+function GnosisLogoBackground() {
+  return (
+    <Image
+      src="/gnosis-logo-bg.png"
+      alt="Gnosis logo"
+      className="absolute top-0 left-0 w-auto h-auto duration-300 group-hover:opacity-100 opacity-0 transition-all z-0"
+      width="222"
+      height="191"
+    />
   );
 }
--- src/app/bakery/components/Banners/Shared.tsx
+++ src/app/bakery/components/Banners/Shared.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 export function BannerContainer({ children }: { children: ReactNode }) {
@@ -6,8 +7,8 @@ export function BannerContainer({ children }: { children: ReactNode }) {
 
 export function BannerCardLarge({ children }: { children: ReactNode }) {
   return (
-    <div className="hidden gap-4 px-7 py-4 w-full items-center sm:flex max-w-[30rem] border-breadgray-light-grey dark:border-transparent border hover:border-breadviolet-shaded hover:dark:border-breadpink-300 m-auto rounded-xl bg-breadgray-ultra-white dark:bg-breadgray-charcoal transition-all group relative overflow-hidden">
-      {children}
+    <div className="hidden gap-4 px-7 py-4 w-full sm:flex max-w-[30rem] border-breadgray-light-grey dark:border-transparent border hover:border-breadviolet-shaded hover:dark:border-breadpink-300 rounded-xl bg-breadgray-ultra-white dark:bg-breadgray-charcoal transition-all group relative overflow-hidden">
+      <div className="flex items-center w-full gap-4">{children}</div>
     </div>
   );
 }
@@ -22,7 +23,7 @@ export function BannerCardSmall({ children }: { children: ReactNode }) {
 
 export function BannerTitle({ children }: { children: string }) {
   return (
-    <div className="text-breadgray-grey300 dark:text-white text-2xl font-medium">
+    <div className="text-breadgray-grey300 dark:text-white text-2xl font-bold">
       {children}
     </div>
   );
@@ -36,30 +37,47 @@ export function BannerDescription({ children }: { children: ReactNode }) {
   );
 }
 
-export function BannerHighlight({
-  icon,
+export function BannerLargeTextContainer({
   children,
 }: {
+  children: ReactNode;
+}) {
+  return <div className="flex flex-col gap-1 grow">{children}</div>;
+}
+
+export function BannerHighlight({
+  children,
+  variant,
+  featureIcon,
+  arrowIcon,
+}: {
+  variant: "pink" | "green";
   children: string;
-  icon: ReactNode;
+  featureIcon: ReactNode;
+  arrowIcon: ReactNode;
 }) {
   return (
-    <div className="flex items-center gap-2 rounded-2xl text-xl px-7 py-3 bg-gnosis-green font-medium text-white">
-      <span className="">{icon}</span>
+    <div
+      className={clsx(
+        "flex items-center gap-2 rounded-2xl text-xl px-7 py-3 font-bold tracking-wider",
+        variant === "green" && "bg-gnosis-green text-white",
+        variant === "pink" &&
+          "bg-breadviolet-shaded dark:bg-breadpink-shaded text-white dark:text-breadgray-grey100"
+      )}
+    >
+      {featureIcon}
       <span>{children}</span>
-      <span className="ml-auto dark:fill-white">
-        <ArrowIcon fill="white" />
-      </span>
+      <span className="ml-auto">{arrowIcon}</span>
     </div>
   );
 }
 
-export function ArrowIcon({ fill = "" }) {
+export function ArrowIcon() {
   return (
     <svg
       width="16"
       height="15"
-      className="group-hover:fill-breadviolet-shaded group-hover:dark:fill-breadpink-300 fill-breadgray-rye dark:fill-breadgray-grey transition-colors"
+      className="fill-current"
       viewBox="0 0 16 15"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -67,7 +85,6 @@ export function ArrowIcon({ fill = "" }) {
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        fill={fill ? fill : ""}
         d="M2.62268e-07 6.5L3.49691e-07 8.5L12 8.5L12 10.5L14 10.5L14 8.5L16 8.5L16 6.5L14 6.5L14 4.5L12 4.5L12 6.5L2.62268e-07 6.5ZM10 2.5L12 2.5L12 4.5L10 4.5L10 2.5ZM10 2.5L8 2.5L8 0.5L10 0.5L10 2.5ZM10 12.5L12 12.5L12 10.5L10 10.5L10 12.5ZM10 12.5L8 12.5L8 14.5L10 14.5L10 12.5Z"
       />
     </svg>
