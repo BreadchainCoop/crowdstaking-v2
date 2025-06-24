e46bc6b632d48c1a297a177dc6c4992a031955ca
Sara <sara@Saras-MacBook-Pro.local>
2025-06-24 13:13:53 +0200

    fix sentry error?

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -24,4 +24,12 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN)
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
+
+    // Add this to prevent multiple instances
+    beforeSend(event) {
+      if (event.exception) {
+        console.log("Sentry event:", event);
+      }
+      return event;
+    },
   });
