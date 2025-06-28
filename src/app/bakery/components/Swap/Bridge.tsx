35d5d7ea921c40176d0fdcdb55e7c06e336f4994
Olaleye Blessing <Olayinkablexxy@gmail.com>
2025-06-28 11:40:43 +0100

    allow users to break bread after bridging

--- src/app/bakery/components/Swap/Bridge.tsx
+++ src/app/bakery/components/Swap/Bridge.tsx
@@ -1,12 +1,62 @@
 "use client";
 
-import type { WidgetConfig } from "@lifi/widget";
-import { LiFiWidget, WidgetSkeleton } from "@lifi/widget";
+import type { Route, RouteExecutionUpdate, WidgetConfig } from "@lifi/widget";
+import {
+  LiFiWidget,
+  useWidgetEvents,
+  WidgetEvent,
+  WidgetSkeleton,
+} from "@lifi/widget";
 import { LiFiWrapper } from "./LiFiWrapper";
+import { useEffect } from "react";
+import { useModal } from "@/app/core/context/ModalContext";
+import { useToast } from "@/app/core/context/ToastContext/ToastContext";
 
 export function Bridge() {
+  /*
+  From LiFi Docs (https://docs.li.fi/widget/widget-events#list-of-events)
+  To minimize unnecessary re-renders and prevent potential glitches in the main Widget component, please integrate the useWidgetEvents hook outside of the component where the main LiFiWidget is integrated.
+  */
+  const widgetEvents = useWidgetEvents();
+  const { setModal } = useModal();
+  const { toastDispatch } = useToast();
+
+  useEffect(() => {
+    const onRouteExecutionCompleted = (route: Route) => {
+      setModal({ type: "LIFI_BRIDGE", route });
+    };
+    const onRouteExecutionFailed = (update: RouteExecutionUpdate) => {
+      console.log("__ LIFI BRIDGE FIALED __", update.process, {
+        error: update.process.error,
+        message: update.process.message,
+      });
+      toastDispatch({
+        type: "CUSTOM",
+        payload: {
+          variant: "error",
+          message: update.process.error?.message || "Transaction failed, try again later!",
+        },
+      });
+    };
+
+    widgetEvents.on(
+      WidgetEvent.RouteExecutionCompleted,
+      onRouteExecutionCompleted
+    );
+    widgetEvents.on(
+      WidgetEvent.RouteExecutionFailed,
+      onRouteExecutionFailed
+    );
+
+    return () => widgetEvents.all.clear();
+  }, [widgetEvents]);
+
+  return <LiFiMainWrapper />;
+}
+
+function LiFiMainWrapper() {
   const config = {
-    appearance: "light",
+    appearance: "system",
     // initialize to xDai on Gnosis chain
     toToken: "0x0000000000000000000000000000000000000000",
     toChain: 100,
@@ -16,6 +66,7 @@ export function Bridge() {
         borderRadius: "16px",
       },
     },
+    disabledUI: ["toToken"],
   } as Partial<WidgetConfig>;
 
   return (
