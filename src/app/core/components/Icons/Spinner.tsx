c2d72badb98459043c568e016b0ba3674a8329eb
Olaleye Blessing <Olayinkablexxy@gmail.com>
2025-11-18 12:40:31 +0100

    Use common loading indicator

--- src/app/core/components/Icons/Spinner.tsx
+++ src/app/core/components/Icons/Spinner.tsx
@@ -1,13 +1,16 @@
+import CircularProgress from "@/app/components/circular-progress";
+
 export function Spinner() {
-  return (
-    <svg
-      className="stroke-current fill-current"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g className="spinner_QPB9">
-        <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="2"></circle>
-      </g>
-    </svg>
-  );
+  return <CircularProgress />
+  // return (
+  //   <svg
+  //     className="stroke-current fill-current"
+  //     viewBox="0 0 24 24"
+  //     xmlns="http://www.w3.org/2000/svg"
+  //   >
+  //     <g className="spinner_QPB9">
+  //       <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="2"></circle>
+  //     </g>
+  //   </svg>
+  // );
 }
