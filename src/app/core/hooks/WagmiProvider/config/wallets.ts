017afc9297af0f85c5172aff059f3bd363fabc7e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2025-12-12 18:13:19 +0100

    Resolve wallet connect error

--- src/app/core/hooks/WagmiProvider/config/wallets.ts
+++ src/app/core/hooks/WagmiProvider/config/wallets.ts
@@ -8,14 +8,22 @@ import {
   safeWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 
+// https://github.com/rainbow-me/rainbowkit/issues/2476#issuecomment-3117608183
 export function getWallets() {
-  return [
-    injectedWallet,
-    metaMaskWallet,
-    walletConnectWallet,
-    frameWallet,
-    rabbyWallet,
-    coinbaseWallet,
-    safeWallet,
-  ];
+	const wallets = [
+		injectedWallet,
+		// metaMaskWallet,
+		// walletConnectWallet,
+		frameWallet,
+		rabbyWallet,
+		coinbaseWallet,
+		safeWallet,
+	];
+
+	if (typeof indexedDB !== "undefined") {
+		// @ts-expect-error Correct
+		wallets.unshift(metaMaskWallet, walletConnectWallet);
+	}
+
+	return wallets;
 }
