023b0c697d17eaa6361509e0cc668cd1f29e2dca
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-01-26 18:17:59 +0100

    Update opengraph metadata

--- src/app/governance/page.tsx
+++ src/app/governance/page.tsx
@@ -1,10 +1,4 @@
 import { GovernancePage } from "./GovernancePage";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Bread Voting",
-  description: "Bake and burn BREAD. Fund post-capitalist web3.",
-};
 
 export default function Governance() {
   return <GovernancePage />;
